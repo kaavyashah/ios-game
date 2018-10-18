dcd070fcefa3c4cb7bcb526c3fab186ae40a3bde
kaavyashah <kaavyashah@berkeley.edu>
2018-10-18 01:54:54 -0700

    almost everything except for game ending

--- codeology game/GamePlayViewController.swift
+++ codeology game/GamePlayViewController.swift
@@ -10,35 +10,54 @@ import UIKit
 
 class GamePlayViewController: UIViewController {
     
+    //connection to the scoreLabel and score value
+    @IBOutlet weak var scoreLabel: UILabel!
+    var score = 0
     
+    //connection to the timerLabel and timer values
     @IBOutlet weak var timerLabel:UILabel!
+    
+    @IBAction func reloadGame(_ sender: Any) {
+        score = 0
+        scoreLabel.text = String(score)
+        startNewRound()
+    }
+    
+    
+    var timer = Timer()
+    var seconds = 11
+    
     //positions for all squares
-    var topLeft = [30, 275]
-    var topMiddle = [140, 275]
-    var topRight = [250, 275]
-    var middleLeft = [30, 385]
-    var middleMiddle = [140, 385]
-    var middleRight = [250, 385]
-    var bottomLeft = [30, 495]
-    var bottomMiddle = [140, 495]
-    var bottomRight = [250, 495]
+    var topLeft = (30.0, 275.0)
+    var topMiddle = (140.0, 275.0)
+    var topRight = (250.0, 275.0)
+    var middleLeft = (30.0, 385.0)
+    var middleMiddle = (140.0, 385.0)
+    var middleRight = (250.0, 385.0)
+    var bottomLeft = (30.0, 495.0)
+    var bottomMiddle = (140.0, 495.0)
+    var bottomRight = (250.0, 495.0)
+    
+    //dictionary mapping color block string name to a position array
+    lazy var positions : [String : (Double, Double)] = ["tLColor" : topLeft, "tMColor" : topMiddle, "tRColor" : topRight, "mLColor" : middleLeft, "mMColor" : middleMiddle, "mRColor" : middleRight, "bLColor" : bottomLeft, "bMColor" : bottomMiddle, "bRColor" : bottomRight]
+    
+    //size of each block
+    var blockSize = 100.0
     
     //hue difference value, if time allows
-    var difference = 0.5;
+    var difference = 0.5
     
     //how to know if the game should end
-    var gameOver = false;
+    var gameOver = false
+    
+    //global random block
+    var chosenBlock = " "
     
     //where the touch occurs
     var touchPoint : (Double, Double) = (0.0, 0.0)
     
     //dictionary mapping from string to UIColor
     var colorVals: [String: UIColor] = ["tLColor": UIColor(red: 0, green: 0, blue: 0, alpha: 1), "tMColor" : UIColor(red: 0, green: 0, blue: 0, alpha: 1), "tRColor" : UIColor(red: 0, green: 0, blue: 0, alpha: 1), "mLColor" : UIColor(red: 0, green: 0, blue: 0, alpha: 1), "mMColor" : UIColor(red: 0, green: 0, blue: 0, alpha: 1), "mRColor" : UIColor(red: 0, green: 0, blue: 0, alpha: 1), "bLColor" : UIColor(red: 0, green: 0, blue: 0, alpha: 1), "bMColor" : UIColor(red: 0, green: 0, blue: 0, alpha: 1), "bRColor" : UIColor(red: 0, green: 0, blue: 0, alpha: 1)]
-    
-    //timer values
-    var timer = Timer()
-    var seconds = 60
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -66,6 +85,8 @@ class GamePlayViewController: UIViewController {
         for i in colorVals.keys {
             colorVals[i] = allColor
         }
+        
+        
         var newColor = randomColor()
         while (newColor == allColor) {
             newColor = randomColor()
@@ -75,16 +96,22 @@ class GamePlayViewController: UIViewController {
     
     //called to start a new round
     func startNewRound() {
+        //reset time seconds to be 11
+        seconds = 11
+        timerUpdate()
+        
         //determines random index for the randomly colored block
         let randomIndex = Int(arc4random_uniform(UInt32(colorVals.count)))
         
         //finds the name of the randomly colored block
         let offsetColor = Array(colorVals.keys)[randomIndex]
+        chosenBlock = offsetColor
         
         //colors all blocks
         fillColors(offset: offsetColor)
         
         //starts the timer
+        timer = Timer()
         timer = Timer.scheduledTimer(timeInterval: 1,target: self, selector: #selector(timerUpdate), userInfo: nil, repeats: true)
         
         showGrid()
@@ -92,21 +119,21 @@ class GamePlayViewController: UIViewController {
     
     //displays the block grid
     func showGrid() {
-        createBlock(xPos: topLeft[0], yPos: topLeft[1], color: colorVals["tLColor"]!)
-        createBlock(xPos: topMiddle[0], yPos: topMiddle[1], color: colorVals["tMColor"]!)
-        createBlock(xPos: topRight[0], yPos: topRight[1], color: colorVals["tRColor"]!)
+        createBlock(xPos: topLeft.0, yPos: topLeft.1, color: colorVals["tLColor"]!)
+        createBlock(xPos: topMiddle.0, yPos: topMiddle.1, color: colorVals["tMColor"]!)
+        createBlock(xPos: topRight.0, yPos: topRight.1, color: colorVals["tRColor"]!)
         
-        createBlock(xPos: middleLeft[0], yPos: middleLeft[1], color: colorVals["mLColor"]!)
-        createBlock(xPos: middleMiddle[0], yPos: middleMiddle[1], color: colorVals["mMColor"]!)
-        createBlock(xPos: middleRight[0], yPos: middleRight[1], color: colorVals["mRColor"]!)
+        createBlock(xPos: middleLeft.0, yPos: middleLeft.1, color: colorVals["mLColor"]!)
+        createBlock(xPos: middleMiddle.0, yPos: middleMiddle.1, color: colorVals["mMColor"]!)
+        createBlock(xPos: middleRight.0, yPos: middleRight.1, color: colorVals["mRColor"]!)
         
-        createBlock(xPos: bottomLeft[0], yPos: bottomLeft[1], color: colorVals["bLColor"]!)
-        createBlock(xPos: bottomMiddle[0], yPos: bottomMiddle[1], color: colorVals["bMColor"]!)
-        createBlock(xPos: bottomRight[0], yPos: bottomRight[1], color: colorVals["bRColor"]!)
+        createBlock(xPos: bottomLeft.0, yPos: bottomLeft.1, color: colorVals["bLColor"]!)
+        createBlock(xPos: bottomMiddle.0, yPos: bottomMiddle.1, color: colorVals["bMColor"]!)
+        createBlock(xPos: bottomRight.0, yPos: bottomRight.1, color: colorVals["bRColor"]!)
     }
     
     //creates one individual block
-    func createBlock(xPos: Int, yPos: Int, color: UIColor) {
+    func createBlock(xPos: Double, yPos: Double, color: UIColor) {
         let layer = CAShapeLayer()
         layer.path = UIBezierPath(roundedRect: CGRect(x: xPos, y: yPos, width: 100, height: 100), cornerRadius: 15).cgPath
         layer.fillColor = color.cgColor
@@ -122,8 +149,31 @@ class GamePlayViewController: UIViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
         let touch = touches.first!
         let location = touch.location(in: view)
-        print(location.x, location.y)
+        
+        //changes global variable to show the point that was touched
         touchPoint = (Double(location.x), Double(location.y))
+        
+        //determines whether the game eeps going or ends
+        analyzeTouch(point: touchPoint)
+    }
+    
+    func analyzeTouch(point : (Double, Double)) {
+        //bottom left corner of the block
+        let minVals = positions[chosenBlock]!
+        
+        //top right corner of the block
+        let maxVals = (minVals.0 + blockSize, minVals.1 + blockSize)
+        
+        //case when the correct block is picked
+        if minVals <= touchPoint && touchPoint <= maxVals {
+            timer.invalidate()
+            score += 1
+            scoreLabel.text = String(score)
+            startNewRound()
+        }
+        else {
+            //have it go to an end game controller view
+        }
     }
     
 
